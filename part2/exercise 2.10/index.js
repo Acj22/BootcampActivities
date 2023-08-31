e35ce628d7fdb278b8df6cd895fdb3d5ff36e741
Acj22 <133715697+Acj22@users.noreply.github.com>
2023-08-31 15:35:52 -0600

    Create index.js

---
+++ part2/exercise 2.10/index.js
@@ -0,0 +1,38 @@
+import React from "react"
+import  ReactDOM  from "react-dom/client";
+import App from "./App";
+
+const Filter = ({ filter, handleFilterChange }) => (
+  <div>
+    <h3>Filter</h3>
+    <p>Filter to show <input value={filter} onChange={handleFilterChange} /></p>
+  </div>
+);
+
+const PersonForm = ({ newName, handleNameChange, handleNumberChange, addData }) => (
+  <div>
+    <h3>New data</h3>
+    <form onSubmit={addData}>
+      <div>
+        Name: <input value={newName.name} onChange={handleNameChange} />
+        Number: <input value={newName.number} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  </div>
+);
+const ToShow = ({ filteredPersons }) =>(
+    <div>
+        <h3>Numbers</h3>
+         {filteredPersons.map(person => (
+            <p key={person.name}>{person.name} {person.number}</p>
+          ))}
+    </div>    
+)
+
+
+export { Filter, PersonForm, ToShow };
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />)
